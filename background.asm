00d98c4870f14a4510505cf52ed59d9c199d7fd4
SimonAMoore <simonaamoore@gmail.com>
2025-06-12 19:25:11 +0100

    Removed blank line from home data

--- background.asm
+++ background.asm
@@ -7,11 +7,10 @@
     ;     XX   XX   ||             ||   XX   XX   ||             ||   XX   XX   ||             ||   XX   XX   ||             ||   XX   XX   ||             ||   XX   XX
     ;     ==   ==   ,,             ``   ==   ==   ,,             ``   ==   ==   ,,             ``   ==   ==   ,,             ``   ==   ==   ,,             ``   ==   ==  
     ;
-    equb &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07, &07   
     equb &04, &04, &01, &02, &02, &00, &04, &04, &01, &02, &02, &00, &04, &04, &01, &02, &02, &00, &04, &04, &01, &02, &02, &00, &04, &04, &01, &02, &02, &00, &04, &04
     equb &04, &04, &05, &07, &07, &03, &04, &04, &05, &07, &07, &03, &04, &04, &05, &07, &07, &03, &04, &04, &05, &07, &07, &03, &04, &04, &05, &07, &07, &03, &04, &04
     equb &02, &02, &08, &07, &07, &06, &02, &02, &08, &07, &07, &06, &02, &02, &08, &07, &07, &06, &02, &02, &08, &07, &07, &06, &02, &02, &08, &07, &07, &06, &02, &02
-    ; Grass path (offset +96)
+    ; Grass path
     equb &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a, &09, &0a
     equb &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c, &0b, &0c    
 }
@@ -85,8 +84,8 @@
     equd &0f0f0f4f
 
     ; Grass 16x16 Block B
-    equd &ed4f0f1f
-    equd &0f0f1f4f
+    equd &eb4f0f1f
+    equd &4f0f1f4f
     equd &0f0f1f3e
     equd &098fc79f
 
@@ -95,31 +94,12 @@
     equd &030f8fc7
     equd &8fc78f0f
     equd &000f0f0f
-    ; 040c 0808          0100:1100:1000:1000
-    ; 0f0f 0b0f          1111:1111:1011:1111
-    ; 2f1f 0f4f          1131:1113:1111:1311
-    ; 7d3e 8feb          1323:1132:3111:3231
-
-    ; 2f1f 0f4f          1131:1113:1111:1311
-    ; 0f0f a70f          1111:1111:2131:1111
-    ; 1f0f f50f          1113:1111:2323:1111
-    ; 3e9f a70f          1132:3113:2131:1111
-
-    ; 1f3e 8f0f          1113:1132:3111:1111
-    ; 0f1f 0f8f          1111:1113:1111:3111
-    ; 4f0f 97c7          1311:1111:2113:2311
-    ; ed0f 078f          3231:1111:0111:3111
-
-    ; 4f9f c70f          1311:3113:2311:1111
-    ; 1fc7 8f0f          1113:2311:3111:1111
-    ; 0f8f 0f0f          1111:3111:1111:1111
-    ; 0f09 0300          1111:1001:0011:0000
 }
 
 .background_draw_home
 {
-    ldy #&00 : sty TEMP_3       ; y offset for sprite drawing routine
-    ldx #&7f                    ; Total number of sprites to draw
+    ldy #&02 : sty TEMP_3       ; y offset for sprite drawing routine
+    ldx #95                     ; Total number of sprites to draw
 .loop_1
     lda background_data, x      ; Load the sprite index to draw next
     jsr background_draw_sprite  ; Draw sprite on display
@@ -129,11 +109,11 @@
     rts
 }
 
-.background_draw_grass_2
+.background_draw_grass_bars
 {
     ldx #&3F
 .loop_1
-    lda background_data + 128, X
+    lda background_data + 96, X
     jsr background_draw_sprite
     dex
     bpl loop_1
@@ -144,9 +124,9 @@
 .background_draw_grass
 {
     ldy #&18 : sty TEMP_3               ; y offset for sprite drawing routine
-    jsr background_draw_grass_2
+    jsr background_draw_grass_bars
     ldy #&2c : sty TEMP_3               ; y offset for sprite drawing routine
-    jsr background_draw_grass_2
+    jsr background_draw_grass_bars
 
 .exit
     rts
@@ -191,7 +171,7 @@
     lda #HI(SCREEN_ADDR)
     sta TEMP_ADDR_1_HI
 
-    ; Add offset to screen address
+    ; Add offset to screen address + TEMP_3 y-offset
     clc
     lda TEMP_1_LO
     adc TEMP_ADDR_1_LO
