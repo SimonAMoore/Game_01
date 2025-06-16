180a3e9497e7d1c69692f6a7ab7bd886fa9fc590
simonamoore <simonaamoore@gmail.com>
2025-06-16 11:54:17 +0100

    Refactor interrupts.asm to correct scanline calculation and update vertical refresh timing for improved synchronization

--- interrupts.asm
+++ interrupts.asm
@@ -44,13 +44,13 @@
     Adjust = 8                                 ; Adjustment for timer to trigger just before horizontal blanking
     H_Refresh = 64                             ; Horizontal refresh period in microseconds
     V_Refresh = 20000 - 32 - 2                 ; Vertical refresh period in microseconds (-2us T1 load, -32us half scanline)
-    Scanline = (8 + 1) * 8 - 3                 ; Calculate scanline to trigger timer on
+    Scanline = (8 + 12) * 8 - 3                 ; Calculate scanline to trigger timer on
     Setup_Time = Scanline * H_Refresh + Adjust ; Initial time needed for timer to sync with vertical refresh
 
     lda #LO(Setup_Time) : sta SYS_VIA_R4_T1C_L
     lda #HI(Setup_Time) : sta SYS_VIA_R5_T1C_H
-    lda #LO(64*16-2) : sta SYS_VIA_R6_T1L_L
-    lda #HI(64*16-2) : sta SYS_VIA_R7_T1L_H
+    lda #LO(V_Refresh) : sta SYS_VIA_R6_T1L_L
+    lda #HI(V_Refresh) : sta SYS_VIA_R7_T1L_H
 
 .exit
     rts
