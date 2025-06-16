6813228a92321dc7b4131638774ca3fac6d17e99
simonamoore <simonaamoore@gmail.com>
2025-06-16 10:53:11 +0100

    Refactor main.asm to improve sine table calculation and streamline main loop structure

--- main.asm
+++ main.asm
@@ -8,7 +8,7 @@ ORG     &1800
 .init_sine_table
 {
     FOR n, 0, 255
-      equb INT((SIN(n * 2 * PI / 256)+1) * 32.5)
+      equb INT((SIN(n * 2 * PI / 256) + 1) * 32.25)
     NEXT
 }
 
@@ -28,7 +28,6 @@ include "background.asm"
     jsr init_text_mode
     jsr instructions
     jsr clear_screen
-    ;jsr test_screen
     jsr init_graphics_mode
     jsr test_chars
     jsr test_sprites
@@ -37,28 +36,7 @@ include "background.asm"
     jsr background_draw_grass
     jsr init_scanline_timer
     jsr init_keyboard
-
-    lda #&00 : sta &00
-
-    ; Wait for vsync
-    {
-        lda #&02
-    .wait
-        bit SYS_VIA_R13_IFR
-        beq wait
-        sta SYS_VIA_R13_IFR
-    }
-
-    ; Set Rupture Screen 0
-    jsr rupture_R0
-
-    ; Reset total lines so original timing ends
-    lda #&04 : sta CRTC_REG
-    lda #&26 : sta CRTC_DATA
-
-    ; Reset T1 interrupt flag
-    lda #&40
-    sta SYS_VIA_R13_IFR
+    ;jsr rupture_init
 
     ; Main loop
 .main_loop
@@ -71,9 +49,9 @@ include "background.asm"
         sta SYS_VIA_R13_IFR
     }
 
-    ; Set rupture screen 1
-    jsr rupture_R1
-    inc &00
+    ; Set timer latch to 2 character lines
+    lda #LO(1022) : sta SYS_VIA_R6_T1L_L
+    lda #HI(1022) : sta SYS_VIA_R7_T1L_H
 
     ; Wait for Scanline_Timer
     {
@@ -84,8 +62,23 @@ include "background.asm"
         sta SYS_VIA_R13_IFR
     }
 
+    ; Set background colour
+    ULA_SET_PALETTE 0, COL_WHITE
+
+    ; Wait for Scanline_Timer
+    {
+        lda #&40
+    .wait
+        bit SYS_VIA_R13_IFR
+        beq wait
+        sta SYS_VIA_R13_IFR
+    }
+
+    ; Set background colour
+    ULA_SET_PALETTE 0, COL_BLACK
+
     ; Set rupture screen 1
-    jsr rupture_R0
+    ;jsr rupture_R1
 
     ; Wait for VSYNC
     lda #&02
@@ -94,12 +87,19 @@ include "background.asm"
     beq wait_vsync
     sta SYS_VIA_R13_IFR
 
+    ; Set background colour
+    ULA_SET_PALETTE 0, COL_BLACK
+
     ; Set rupture screen 0
-    jsr rupture_R0
+    ;jsr rupture_R0
 
 .vertical_blanking
     ;jsr read_keys
     ;jsr test_keys
+
+    ; Reset T1 counter
+    lda #&40
+    sta SYS_VIA_R13_IFR
 
     ; Return to start of main loop
     jmp main_loop
