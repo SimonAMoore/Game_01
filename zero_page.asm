c06f17d0e472039f292f41f60ee357765331a730
simonamoore <simonaamoore@gmail.com>
2025-06-16 10:53:23 +0100

    Refactor zero_page.asm to reorganize temporary storage definitions for improved clarity and maintainability

--- zero_page.asm
+++ zero_page.asm
@@ -1,29 +1,34 @@
 ; Zero Page
-ORG &70
-GUARD &9F
+ORG     &00
+GUARD   &ff
 
-.TEMP_1_LO        skip 1
-.TEMP_1_HI        skip 1
-.TEMP_2_LO        skip 1
-.TEMP_2_HI        skip 1
-.TEMP_3_LO        skip 1
-.TEMP_3_HI        skip 1
+.RUPTURE_COUNTER skip 1
+
+; Register temporary storage
+.TEMP_A skip 1
+.TEMP_X skip 1
+.TEMP_Y skip 1
+
+; 16-bit temporary storage
+.TEMP_1_LO skip 1
+.TEMP_1_HI skip 1
+.TEMP_2_LO skip 1
+.TEMP_2_HI skip 1
+.TEMP_3_LO skip 1
+.TEMP_3_HI skip 1
 
 TEMP_1 = TEMP_1_LO
 TEMP_2 = TEMP_2_LO
 TEMP_3 = TEMP_3_LO
 
-.TEMP_ADDR_1_LO   skip 1
-.TEMP_ADDR_1_HI   skip 1
-.TEMP_ADDR_2_LO   skip 1
-.TEMP_ADDR_2_HI   skip 1
-.TEMP_ADDR_3_LO   skip 1
-.TEMP_ADDR_3_HI   skip 1
+; Memory address temporary storage
+.TEMP_ADDR_1_LO skip 1
+.TEMP_ADDR_1_HI skip 1
+.TEMP_ADDR_2_LO skip 1
+.TEMP_ADDR_2_HI skip 1
+.TEMP_ADDR_3_LO skip 1
+.TEMP_ADDR_3_HI skip 1
 
 TEMP_ADDR_1 = TEMP_ADDR_1_LO
 TEMP_ADDR_2 = TEMP_ADDR_2_LO
 TEMP_ADDR_3 = TEMP_ADDR_3_LO
-
-.TEMP_A           skip 1
-.TEMP_X           skip 1
-.TEMP_Y           skip 1
